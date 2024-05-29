9ca62ca53182d705659ce3c1e5b228ceb16f3212
maazcubix <maaz.jabbar@cubixlabs.com>
2024-05-29 16:18:29 +0500

    multer-completed

--- models/user.model.js
+++ models/user.model.js
@@ -41,7 +41,7 @@ const userSchema=new Schema ({
         type:String,
         required:[true,"password is required"]
     },
-    refreshTOken:{
+    refreshToken:{
             type:String
     }
 
